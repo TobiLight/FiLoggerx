87eb471381b5970e523e63f615e8acaa7d0efc97
TobiLight <jgetitdonefast@gmail.com>
2023-03-24 00:27:05 +0100

    Finance logger

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { Invoice } from "./classes/Invoice.js"
+import { Payment } from './classes/Payment.js'
+import { ListTemplate } from "./classes/ListTemplate.js"
+import { HasFormatter } from './interfaces/HasFormatter' 
+
+const form = document.querySelector('.new-item-form') as HTMLFormElement
+
+const type = document.querySelector('#type') as HTMLSelectElement
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement
+const details = document.querySelector('#details') as HTMLInputElement
+const amount = document.querySelector('#amount') as HTMLInputElement
+
+const ul = document.querySelector('ul') as HTMLUListElement
+const list = new ListTemplate(ul)
+
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault()
+
+  type Values = [string, string, number]
+  let values: Values = [tofrom.value, details.value, amount.valueAsNumber]
+
+  let doc: HasFormatter
+  if(type.value === 'invoice'){
+    doc = new Invoice(...values)
+  }else{
+    doc = new Payment(...values)
+  } 
+
+  list.render(doc, type.value, 'end')
+})
